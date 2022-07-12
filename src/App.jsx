11bf32bb41c0ab94324fbbf4c541467ee39d509e
Omar-Tabaa <omar.tabaa90@gmail.com>
2022-07-12 15:54:42 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.scss";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const backend_url = "http://localhost:3044/job-sources";
+
+//import.meta.env.VITE_BACKEND_URL;
+
+function App() {
+    const [jobSources, setJobSources] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            setJobSources((await axios.get(backend_url)).data);
+        })();
+    }, []);
+    return (
+        <div className="App">
+            <h1>EJT Job Manager</h1>
+            <p>There are {jobSources.length} job sources:</p>
+            <ul>
+                {jobSources.map((jobSource, i) => {
+                    return <li key={i}>{jobSource.name}</li>;
+                })}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
